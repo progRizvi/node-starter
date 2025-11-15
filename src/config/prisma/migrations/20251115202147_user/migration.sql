7ef6c10f463fa01db6ddb1bf8fa9617ee25cc2e6
progRizvi <prog.rizvi@gmail.com>
2025-11-16 02:23:12 +0600

    feat: add nodemailer dependency and update user model
    - Added nodemailer to dependencies for email functionality.
    - Removed old migration file for users table.
    - Updated User model in schema.prisma to include new fields: isVerified, verificationToken, resetToken, resetTokenExpiry, and refreshToken.
    - Created new migration file for users table with updated schema.

--- src/config/prisma/migrations/20251115202147_user/migration.sql
+++ src/config/prisma/migrations/20251115202147_user/migration.sql
@@ -1,12 +1,17 @@
 -- CreateTable
-CREATE TABLE `User` (
+CREATE TABLE `users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
     `email` VARCHAR(255) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
+    `isVerified` BOOLEAN NOT NULL DEFAULT false,
+    `verificationToken` VARCHAR(255) NULL,
+    `resetToken` VARCHAR(255) NULL,
+    `resetTokenExpiry` DATETIME(3) NULL,
+    `refreshToken` VARCHAR(255) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `users_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
