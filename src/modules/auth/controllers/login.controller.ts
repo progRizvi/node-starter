a1e2b1ec500f4e6d13192a3b31f8f8f6d3067a5e
progRizvi <prog.rizvi@gmail.com>
2025-11-16 15:30:58 +0600

    feat: implement refresh token and logout functionality in authentication

--- src/modules/auth/controllers/login.controller.ts
+++ src/modules/auth/controllers/login.controller.ts
@@ -1,3 +1,4 @@
+import { jwt } from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { log } from "@utils/logger";
 import { Request, Response } from "express";
@@ -65,4 +66,76 @@ export const login = async (req: Request, res: Response) => {
 		log.error("Login Error:" + error);
 		return res.status(500).json({ message: "Internal Server Error" });
 	}
+};
+
+export const refreshToken = async (req: Request, res: Response) => {
+	const { refreshToken } = req.body;
+
+	if (!refreshToken) {
+		return res
+			.status(400)
+			.json({ message: "Refresh Token is required", success: false });
+	}
+
+	let decoder;
+	try {
+		decoder = jwt.verify(
+			refreshToken,
+			process.env.JWT_REFRESH_SECRET || "refresh_secret"
+		);
+	} catch (error) {
+		log.error("Error verifying refresh token: " + error);
+		return res
+			.status(401)
+			.json({ message: "Invalid Refresh Token", success: false });
+	}
+
+	if (!decoder) {
+		res.status(401).json({ message: "Invalid Refresh Token", success: false });
+	}
+
+	const user = await prisma.user.findFirst({
+		where: { id: parseInt((decoder as any).userId) },
+	});
+
+	if (!user || user.refreshToken !== refreshToken) {
+		return res
+			.status(401)
+			.json({ message: "Invalid Refresh Token", success: false });
+	}
+
+	const newAccessToken = generateAccessToken(user.id, user.email);
+	const newRefreshToken = generateRefreshToken(user.id);
+
+	await prisma.user.update({
+		where: { id: user.id },
+		data: { refreshToken: newRefreshToken },
+	});
+
+	return res.status(200).json({
+		success: true,
+		message: "Refresh Token Updated Successfully",
+		data: {
+			accessToken: newAccessToken,
+			refreshToken: newRefreshToken,
+		},
+	});
+};
+
+export const logout = async (req: Request, res: Response) => {
+	const userId = req.user?.id;
+
+	if (!userId) {
+		return res.status(401).json({ message: "Unauthorized", success: false });
+	}
+
+	await prisma.user.update({
+		where: { id: userId },
+		data: { refreshToken: null },
+	});
+
+	return res.status(200).json({
+		success: true,
+		message: "Logout Successful",
+	});
 };
--- src/modules/auth/routes/auth.routes.ts
+++ src/modules/auth/routes/auth.routes.ts
@@ -4,7 +4,7 @@ import {
 	authLimiter,
 	registerLimiter,
 } from "@middlewares/reteLimiting.middleware";
-import { login } from "../controllers/login.controller";
+import { login, logout, refreshToken } from "../controllers/login.controller";
 import { verifyToken } from "../controllers/verifyToken.controller";
 import { userProfile } from "../controllers/userProfile.controller";
 import { authentication } from "@middlewares/authentication";
@@ -14,5 +14,9 @@ const router = express.Router();
 router.post("/register", registerLimiter, register);
 router.post("/login", authLimiter, login);
 router.get("/verify-email/:token", authLimiter, verifyToken);
+router.post("/refresh-token", authLimiter, refreshToken);
+router.post("/logout", authLimiter, authentication, logout);
+
 router.get("/user-profile", [authLimiter, authentication], userProfile);
+
 export default router;
